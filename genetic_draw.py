46f04e6e27ae88bc2bdd0e8d7b015e8989c3978c
Mateusz Janda <mateusz.janda@gmail.com>
2019-04-29 10:21:35 +0200

    libncurses 6.1 call

--- genetic_draw.py
+++ genetic_draw.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+import time
+import ctypes
+from ctypes import cdll
+
+# https://docs.python.org/2.5/lib/ctypes-loading-dynamic-link-libraries.html
+curses = ctypes.CDLL('libncursesw.so.6.1')
+# curses = ctypes.CDLL('libncurses.so.5')
+
+# http://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/
+scr = curses.initscr()
+curses.start_color()
+
+# curses.printw("asdf")
+curses.printw("asdf".encode('utf-8'))
+curses.printw("asdf")
+curses.mvprintw(1, 1, "jkl".encode('utf-8'))
+curses.refresh()
+
+curses.getch()
+
+curses.endwin()

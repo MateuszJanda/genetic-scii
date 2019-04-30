190e6bbea916f82a2046c299468a6dc5f01947ad
Mateusz Janda <mateusz.janda@gmail.com>
2019-04-30 20:25:44 +0200

    Colors init

--- genetic_draw.py
+++ genetic_draw.py
@@ -4,6 +4,24 @@ import time
 import ctypes
 from ctypes import cdll
 
+#
+# Definitions from curses.h
+#
+
+NCURSES_ATTR_SHIFT = 8
+
+
+def NCURSES_BITS(mask, shift):
+    return mask << (shift + NCURSES_ATTR_SHIFT)
+
+
+A_COLOR = NCURSES_BITS((1 << 8) - 1, 0)
+
+
+def COLOR_PAIR(n):
+    return NCURSES_BITS(n, 0) & A_COLOR
+
+
 # https://docs.python.org/2.5/lib/ctypes-loading-dynamic-link-libraries.html
 curses = ctypes.CDLL('libncursesw.so.6.1')
 # curses = ctypes.CDLL('libncurses.so.5')
@@ -12,10 +30,19 @@ curses = ctypes.CDLL('libncursesw.so.6.1')
 scr = curses.initscr()
 curses.start_color()
 
+curses.init_extended_color(2, 999, 0, 0)
+curses.init_extended_color(3, 0, 999, 0)
+
+curses.init_extended_pair(2, 2, 3)
+
 # curses.printw("asdf")
 curses.printw("asdf".encode('utf-8'))
 curses.printw("asdf")
+
+curses.attron(COLOR_PAIR(2))
 curses.mvprintw(1, 1, "jkl".encode('utf-8'))
+curses.attroff(COLOR_PAIR(2))
+
 curses.refresh()
 
 curses.getch()

cbf95190b175c8950e3e0ac06f1c6772ff52b88a
Mateusz Janda <mateusz.janda@gmail.com>
2019-10-19 00:51:50 +0200

    Calculation text rectange

--- genetic_draw.py
+++ genetic_draw.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import random
 from PIL import Image, ImageDraw, ImageFont
-
 
 STEPS = 1
 SPEC_CNT = 100
 
+
 WIDTH = 400
 HEIGHT = 400
+
+
 
 BLACK = 0
 WHITE = 255
@@ -17,9 +20,14 @@ FONT_SIZE = 17
 
 
 
-def mutate():
+def mutate(dna):
+    # dna_to_img()
+    x = random.randint(0, WIDTH//FONT_SIZE//2 - 1)
+    y = random.randint(0, HEIGHT//FONT_SIZE - 1)
+    width = random.randint(1, WIDTH//FONT_SIZE//2 - x)
+    char = random.choice('abcd')
+
     dna_to_img()
-    pass
 
 
 def score():
@@ -36,17 +44,37 @@ def dna_to_img():
     font = ImageFont.truetype(FONT_NAME, size=FONT_SIZE)
 
     text = "to jest tekst\n   i trzy spacje"
+    text = "this text is to looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong"
 
-    draw.text(xy=(0, 0), text=text, font=font, fill=WHITE)
+    # print(font.getsize("a"))
+    print(draw.textsize("a", font))
+    print(draw.textsize("asdf\nasdf", font))
+    # print(draw.multiline_textsize("asdf\nasdf", font))
+
+    # draw.text(xy=(0, 0), text=text, font=font, fill=WHITE)
+
+    # default spacing 4
+
+    draw.text(xy=(0, 0), text="╣ ╣", font=font, fill=WHITE)
+    draw.text(xy=(0, 15+4), text="asdf", font=font, fill=WHITE)
+    draw.text((0,0), text="asdf\nasdf", font=font, fill=WHITE)
+    # draw.text(xy=(0, 0), text="xxx\njkl", font=font, fill=WHITE)
+
+    # draw.multiline_text((0,0), text="asdf\nasdf", font=font, fill=WHITE)
+
     img.save("out.png")
 
 
 def main():
+    dna = []
+    for _ in range(SPEC_CNT):
+        dna.append([" " for _ in range(WIDTH//FONT_SIZE//2)] for _ in range(HEIGHT//FONT_SIZE))
+
 
     for step in range(STEPS):
         print("Generation:", step)
 
-        mutate()
+        mutate(dna)
 
         score()
 

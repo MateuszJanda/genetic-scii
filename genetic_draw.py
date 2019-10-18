cbdbed85efb292f5fa41afc2a966758979b9cb71
Mateusz Janda <mateusz.janda@gmail.com>
2019-10-18 23:08:55 +0200

    Pillow usage

--- genetic_draw.py
+++ genetic_draw.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
 
@@ -8,6 +9,13 @@ SPEC_CNT = 100
 
 WIDTH = 100
 HEIGHT = 100
+
+BLACK = 0
+WHITE = 255
+
+FONT_NAME = 'UbuntuMono-R'
+FONT_SIZE = 17
+
 
 
 def mutate():
@@ -23,12 +31,17 @@ def corss():
 
 
 def dna_to_img():
-    pass
+    pil_img = Image.new('RGB', color=BLACK, size=(WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(pil_img)
+    font = ImageFont.truetype(FONT_NAME, size=FONT_SIZE)
+
+    draw.text(xy=(0, 0), text=text, font=font, fill=WHITE)
+    terminal_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
 
 
 def main():
 
-    for step in range(1000):
+    for step in range(STEPS):
         print("Generation:", step)
 
         mutate()

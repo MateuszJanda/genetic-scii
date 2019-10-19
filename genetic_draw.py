a96668dab18177873d079f43cb2f7fca7a5a4f02
Mateusz Janda <mateusz.janda@gmail.com>
2019-10-20 00:47:06 +0200

    Score calculation with numpy

--- genetic_draw.py
+++ genetic_draw.py
@@ -2,6 +2,7 @@
 
 import random
 from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 
 
 STEPS = 1
@@ -29,13 +30,14 @@ class Char:
 def main():
     char_width, char_height = singe_char_size()
     dna = basic_dna(char_width, char_height)
+    orig_img = get_orig_image()
 
     for step in range(STEPS):
         print("Generation:", step)
 
         mutate(dna, char_width, char_height)
 
-        score()
+        score(dna, orig_img, char_width, char_height)
 
         corss()
 
@@ -57,6 +59,45 @@ def basic_dna(char_width, char_height):
     return dna
 
 
+def get_orig_image(path="orig.png"):
+    img = Image.open(path)
+
+    assert img.size == (IMG_WIDTH, IMG_HEIGHT)
+    assert img.mode == "L"
+
+    return img
+
+
+def mutate(dna, char_width, char_height):
+    for d in dna:
+        x = random.randint(0, IMG_WIDTH//char_width - 1)
+        y = random.randint(0, IMG_HEIGHT//char_height - 1)
+        end_x = random.randint(x + 1, IMG_WIDTH//char_width)
+        end_y = random.randint(y + 1, IMG_HEIGHT//char_height)
+
+        foreground = random.randint(0, 255)
+        background = random.randint(0, 255)
+        symbol = random.choice('abcd')
+
+        for row in range(y, end_y):
+            for col in range(x, end_x):
+                d[row][col] = Char(symbol, foreground, background)
+
+
+def score(dna, orig_img, char_width, char_height):
+    scores = {}
+    for idx, d in enumerate(dna):
+        img = dna_to_img(d, char_width, char_height)
+        result = np.sum(np.abs(np.array(orig_img) - np.array(img)))
+        scores[idx] = result
+
+    return scores
+
+
+def corss():
+    pass
+
+
 def dna_to_img(dna, char_width, char_height):
     img = Image.new("L", color=BLACK, size=(IMG_WIDTH, IMG_HEIGHT))
     draw = ImageDraw.Draw(img)
@@ -71,37 +112,9 @@ def dna_to_img(dna, char_width, char_height):
     return img
 
 
-def dump_dna(dna):
+def print_dna(dna):
     for line in dna:
         print("".join([ch.symbol for ch in line]))
-
-
-def mutate(dna, char_width, char_height):
-    x = random.randint(0, IMG_WIDTH//char_width - 1)
-    y = random.randint(0, IMG_HEIGHT//char_height - 1)
-    end_x = random.randint(x + 1, IMG_WIDTH//char_width)
-    end_y = random.randint(y + 1, IMG_HEIGHT//char_height)
-
-    foreground = random.randint(0, 255)
-    background = random.randint(0, 255)
-    symbol = random.choice('abcd')
-
-    for row in range(y, end_y):
-        for col in range(x, end_x):
-            dna[0][row][col] = Char(symbol, foreground, background)
-
-    # dump_dna(dna[0])
-
-    img = dna_to_img(dna[0], char_width, char_height)
-    # img.save("out.png")
-
-
-def score():
-    pass
-
-
-def corss():
-    pass
 
 
 if __name__ == '__main__':

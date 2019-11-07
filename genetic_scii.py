46995b82493251a6c9d232f3935a48950415e49a
Mateusz Janda <mateusz.janda@gmail.com>
2019-11-07 10:44:35 +0100

    Fixed cross - dna and img cloning

--- genetic_scii.py
+++ genetic_scii.py
@@ -44,7 +44,8 @@ class Char:
 def main():
     random.seed(1321)
 
-    orig_arr = get_orig_array2()
+    orig_arr = get_orig_array()
+    orig_arr = convert_to_mosaic(orig_arr)
     population = basic_population(orig_arr.shape)
 
     for step in range(STEPS):
@@ -53,10 +54,9 @@ def main():
         mutate(population, CHAR_BASE_BASIC)
         best_idx, scores = select(population, orig_arr)
         population = cross(population, best_idx)
-        # dump_img(population, 0, step)
-        # dump_img(population, 1, step)
-        dump_img(population, best_idx[0], step)
-        # break
+
+        if step % 10:
+            dump_img(population, best_idx[0], step)
 
         print("Generation:", step, "time:", time.time() - tic, "best_idx:", scores[best_idx[0]])
 
@@ -66,7 +66,6 @@ def main():
 def basic_population(img_shape):
     img = Image.new("L", color=BLACK, size=(img_shape[1], img_shape[0]))
     dna = np.full(shape=(img_shape[0]//CHAR_SHAPE[0], img_shape[1]//CHAR_SHAPE[1]), fill_value=Char())
-
     population = [(np.copy(dna), copy.copy(img)) for _ in range(POPULATION_NUM)]
     return population
 
@@ -79,20 +78,13 @@ def get_orig_array(path="orig.png"):
     return np.array(img)
 
 
-def get_orig_array2(path="orig.png"):
-    img = Image.open(path)
-
-    assert img.mode == "L"
-
-    img = np.array(img)
-
+def convert_to_mosaic(arr):
     for x in range(0, img.shape[1], CHAR_SHAPE[1]):
         for y in range(0, img.shape[0], CHAR_SHAPE[0]):
             img[y:y+CHAR_SHAPE[0], x:x+CHAR_SHAPE[1]] = np.average(img[y:y+CHAR_SHAPE[0], x:x+CHAR_SHAPE[1]])
 
-    i = Image.fromarray(img)
-    i.save('blocks.jpg')
     return img
+
 
 def mutate(population, char_base, random_background=True):
     for dna, img in population:
@@ -131,13 +123,14 @@ def select(population, orig_arr):
         scores[idx] = result
 
     best_idx = sorted(scores, key=scores.get)[:BEST_NUM]
-    print(scores[sorted(scores, key=scores.get)[0]], scores[sorted(scores, key=scores.get)[-1]])
 
     return best_idx, scores
 
 
 def cross(population, best_idx):
-    result = [np.copy(population[best_idx[idx % BEST_NUM]]) for idx in range(len(population))]
+    best_img = [copy.deepcopy(population[idx]) for idx in best_idx]
+    result = [copy.deepcopy(best_img[idx % BEST_NUM]) for idx in range(len(population))]
+
     return result
 
 
@@ -147,16 +140,13 @@ def print_dna(dna):
 
 
 def dump_img(population, idx, step):
-    # if step % 10:
-    #     return
-
     dna, img = population[idx]
     img_shape = (img.size[1], img.size[0])
     out_img = dna_to_img(dna, img_shape)
     # out_img.save(str(idx) + "a%04d.png" % step)
     out_img.save("a%04d.png" % step)
 
-    assert np.all(np.array(out_img) == np.array(img))
+    # assert np.all(np.array(out_img) == np.array(img))
 
 
 def dna_to_img(dna, img_shape):

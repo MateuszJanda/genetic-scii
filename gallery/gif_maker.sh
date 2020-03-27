a1c4ec2c481c6b397887e57e511c7267ed971eb9
Mateusz Janda <mateusz.janda@gmail.com>
2020-03-27 16:52:06 +0100

    Added gif_maker

---
+++ gallery/gif_maker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "Warning - missing original file name."
+    echo "Usage:"
+    echo "   ./gif_maker.sh input.png"
+else
+    SCALE="400x400"
+
+    # Shrink original image
+    orig_name=resize_$1
+    convert $1 -resize $SCALE $orig_name
+
+    # Shrink all snapshot_*.png files in current directory and merge with original file
+    for i in snapshot_*.png;
+    do
+        # Resize image
+        snap_name=resize_$i
+        convert $i -resize $SCALE $snap_name
+
+        # Merge original (resized) with snapshot (resized)
+        out_name=out_$i
+
+        convert $orig_name $snap_name +append $out_name
+    done
+
+    PALETTE_FILE="tmp_pallete.png"
+    VIDEO_FILE="output_video.mp4"
+    INPUT_FILES="out_snapshot_%4d.png"
+    OUTPUT_GIF="output.gif"
+    FILTERS="fps=25"
+
+    # Create video from *.png files
+    ffmpeg -r 100 -i $INPUT_FILES -c:v libx264 -crf 0 -preset veryslow $VIDEO_FILE
+
+    # Convert video file to .gif
+    ffmpeg -v warning -i $VIDEO_FILE -vf "$FILTERS,palettegen" -y $PALETTE_FILE
+    ffmpeg -v warning -i $VIDEO_FILE -i $PALETTE_FILE -lavfi "$FILTERS [x]; [x][1:v] paletteuse" -y $OUTPUT_GIF
+fi
